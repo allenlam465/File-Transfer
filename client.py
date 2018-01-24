28634ad673e7f0a27c0ef507dd7a26928f0968a4
Allen Lam <allenlam465@gmail.com>
2018-01-24 12:40:36 -0800

    First commit server and client setup.

---
+++ client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+
+s.connect((host, port))
+print s.recv(1024)
+s.close
---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host,port))
+
+s.listen(5)
+
+while True
+    c, addr = s.accept()
+    print "Got connection from ", addr
+    c.send("Thank you for connecting.")
+    c.close()
