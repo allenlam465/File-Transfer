fe89e94512dae8b9c17bb3c0dee7b15b5b482734
Allen Lam <allenlam465@gmail.com>
2018-01-24 17:11:18 -0800

    Client and server made. Next step is user credentials validation and try to hash it.

--- client.py
+++ client.py
@@ -2,10 +2,10 @@
 
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
+host = socket.gethostname() #Obtain host name
+port = 12345 #Port that it uses
 
-s.connect((host, port))
-print s.recv(1024)
-s.close
+s.connect((host, port)) #Connection with host and port eg. 123.00.000.00:12345
+print s.recv(1024) #Print recieved from server up to 1024 bytes
+s.close #Close connection
--- server.py
+++ server.py
@@ -2,15 +2,18 @@
 
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
-s.bind((host,port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
+host = socket.gethostname() #Obtain host name
+port = 12345 #Port that it uses
+s.bind((host,port)) #binds a socket to address
 
-s.listen(5)
+s.listen(5) #Listen for 5 connections
 
 while True:
-    c, addr = s.accept()
+    (c, addr) = s.accept()  #accept connection in the bound socket
     print "Got connection from ", addr
-    c.send("Thank you for connecting.")
-    c.close()
+    c.send("Thank you for connecting.") #Sends message back to client
+    c.close() #Close connection with client
+    break
+
+s.close() #Close server
