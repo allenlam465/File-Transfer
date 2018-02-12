5c394070eb0d51ad703531bfd99a8ff2f2ec9139
Allen Lam <allenlam465@gmail.com>
2018-02-12 14:28:14 -0800

    Client and server user authenication made. Need improvements.

--- client.py
+++ client.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
 import socket
+import getpass
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
+def login():
+    user = input("Username: ")
+    pw = getpass.getpass()
+    token = user + "/" + pw
+    return token
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
 host = socket.gethostname() #Obtain host name
 port = 12345 #Port that it uses
 
-s.connect((host, port)) #Connection with host and port eg. 123.00.000.00:12345
-print s.recv(1024) #Print recieved from server up to 1024 bytes
-s.close #Close connection
+while True:
+    token = login()
+    server.connect((host, port)) #Connection with host and port eg. 123.00.000.00:12345
+    server.send(bytes(token, 'utf-8'))
+    print (server.recv(1024).decode('utf-8')) #Print recieved from server up to 1024 bytes
+
+server.send(bytes("Sender closing connection.", 'utf-8'))
+server.close #Close connection
--- server.py
+++ server.py
@@ -2,18 +2,39 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
+def checkAuthen(token):
+    fileName = "users.txt"
+    file = open(fileName, "r")
+
+    with open (fileName) as f:
+        for line in f.readlines():
+            if(token.strip() == line.strip()):
+                print ("Authorized user.")
+                return True
+            else:
+                print ("Invalid username or password.")
+                return False
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
 host = socket.gethostname() #Obtain host name
 port = 12345 #Port that it uses
-s.bind((host,port)) #binds a socket to address
+server.bind((host,port)) #binds a socket to address
 
-s.listen(5) #Listen for 5 connections
+server.listen(5) #Listen for 5 connections
 
-while True:
-    (c, addr) = s.accept()  #accept connection in the bound socket
-    print "Got connection from ", addr
-    c.send("Thank you for connecting.") #Sends message back to client
-    c.close() #Close connection with client
-    break
+(client, addr) = server.accept()  #accept connection in the bound socket
+print ("Got connection from ", addr)
+token = (client.recv(1024).decode('utf-8')) #Recv from connected client
 
-s.close() #Close server
+print ("Checking authorization.")
+
+if(checkAuthen(token)):
+    while True:
+        client.send(bytes("Thank you for connecting.",'utf-8')) #Sends message back to client
+        print (client.recv(1024).decode('utf-8'))
+        client.close() #Close connection with client
+        break
+else:
+    client.send(bytes("Invalid login information.",'utf-8')) #Sends message back to client
+
+server.close() #Close server
