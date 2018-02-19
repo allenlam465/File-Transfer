d61254079205e4dee931366f087099f4b5714d25
Allen Lam <allenlam465@gmail.com>
2018-02-18 16:23:36 -0800

    Everything works acc autorization and file sending works now. Need to add other features such as XOR, salt/hash, encrypt/decrypt, simple GUI, and  etc...

--- client.py
+++ client.py
@@ -3,44 +3,50 @@
 import socket
 import getpass
 
+
 def login():
     user = input("Username: ")
     pw = getpass.getpass()
     token = user + "/" + pw
     return token
 
-def ping(hostName):
-    host = hostName
-    response = os.system("ping -c 1" + host)
 
-    if (response == 0):
-        return True
+# Start socket protocols
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()  # Obtain host name
+port = 12345  # Port that it uses
+
+
+server.settimeout(10)
+# Connection with host and port eg. 123.00.000.00:12345
+server.connect((host, port))
+
+while True:
+    token = login()
+    server.send(bytes(token, 'utf-8'))  # sends login token user/pass
+
+    # recieve confirmation of successful login
+    accept = (server.recv(1024).decode('utf-8')).strip()
+
+    if accept == "1":
+        # Print recieved from server up to 1024 bytes
+        print(server.recv(1024).decode('utf-8'))
+
+        fileName = input("File: ")
+
+        fstream = open(fileName, 'rb')
+        sent = fstream.read(1024)
+        while(sent):
+            server.send(sent)
+            print('Sent ', repr(sent))
+            sent = fstream.read(1024)
+        break
+    elif accept == "-1":
+        print(server.recv(1024).decode('utf-8'))
+        break
     else:
-        return False
-
-def sendFile(fileName):
-    with open(fileName, 'rb') as fstream:
-        while True:
-            data = fstream.read(1024)
-            print ("Sending data...")
-            server.send(data)
-            print ("Sent data.")
-            fstream.close()
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
-host = socket.gethostname() #Obtain host name
-port = 12345 #Port that it uses
-
-token = login()
-server.connect((host, port)) #Connection with host and port eg. 123.00.000.00:12345
-
-while ping(host):
-    #print ("What is the file name?")
-    #fileName = input("Filename: ")
-    #sendFile(fileName)
-    server.send(bytes(token, 'utf-8'))
-    print (server.recv(1024).decode('utf-8')) #Print recieved from server up to 1024 bytes
-    break
+        print(server.recv(1024).decode('utf-8'))
+        print("Invalid login information.")  # Sends message back to client
 
 server.send(bytes("Sender closing connection.", 'utf-8'))
-server.close #Close connection
+server.close  # Close connection
--- server.py
+++ server.py
@@ -1,51 +1,88 @@
 #!/usr/bin/env python
 
 import socket
+import getpass
+
+
+def createAcc():
+    user = input("Input new user:")
+    pw = getpass.getpass()
+    token = user + "/" + pw
+
+    with open("user.txt", "a") as myFile:
+        myFile.write(token)
+
 
 def checkAuthen(token):
     fileName = "users.txt"
     file = open(fileName, "r")
 
-    with open (fileName) as f:
+    with open(fileName) as f:
         for line in f.readlines():
             if(token.strip() == line.strip()):
-                print ("Authorized user.")
+                print("Authorized user.")
                 return True
 
     print("Invalid username or password.")
     return False
 
 
+def sendMessage(message):
+    # Sends message back to client on server
+    client.send(bytes(message, 'utf-8'))
+
+
 def serverClose():
-    server.close()
+    sendMessage(" Closing connections.")
+    server.close()  # Close server
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start socket protocols
-host = socket.gethostname() #Obtain host name
-port = 12345 #Port that it uses
-server.bind((host,port)) #binds a socket to address
 
-server.listen(5) #Listen for 5 connections
+# Start socket protocols
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()  # Obtain host name
+port = 12345  # Port that it uses
+server.bind((host, port))  # binds a socket to address
 
-(client, addr) = server.accept()  #accept connection in the bound socket
-print ("Got connection from ", addr)
+server.listen(5)  # Listen for 5 connections
+print("Listening for client.")
 
-tries = 0
+(client, addr) = server.accept()  # accept connection in the bound socket
+print("Got connection from ", addr)
 
-while(tries != 3):
+tries = 3
 
-    token = (client.recv(1024).decode('utf-8')) #Recv from connected client
-    print ("Checking authorization.")
+while(tries != 0):
+    token = (client.recv(1024).decode('utf-8'))  # Recv from connected client
+
+    print("Checking authorization.")
 
     if(checkAuthen(token)):
         while True:
-            client.send(bytes("Thank you for connecting.",'utf-8')) #Sends message back to client
-            print (client.recv(1024).decode('utf-8'))
-            client.close() #Close connection with client
+            sendMessage("1")  # Sends message back to client
+            sendMessage("Thank you for connecting.")
+
+            with open('received_file', 'wb') as f:
+                print("Opened file.")
+                while True:
+                    print('Receiving data...')
+                    data = client.recv(1024)
+                    print('data=%s', (data))
+                    if not data:
+                        break
+                        # write data to a file
+                    f.write(data)
+            f.close()
+            break
+        break
+    else:
+        tries -= 1
+        if tries == 0:
+            sendMessage("-1")
+            sendMessage("Too many tries server closing.")
             break
         else:
-            client.send(bytes("Invalid login information.",'utf-8')) #Sends message back to client
-            tries += 1
+            sendMessage("0")
+            line = "Number tries left: " + str(tries)
+            sendMessage(line)
 
-    server.listen(5) #Listen for 5 connections
-
-server.close() #Close server
+serverClose()
