4e7d2359976f3fb266af871ece408fdb8e756f6b
Allen Lam <allenlam465@gmail.com>
2018-02-22 19:05:55 -0800

    Acc creation now in tool.py.

--- tool.py
+++ tool.py
@@ -2,6 +2,9 @@
 
 from client import Client
 from server import Server
+import getpass
+import crypt
+import hashlib
 
 
 def client():
@@ -18,10 +21,24 @@ def server():
     server.main()
 
 
+def create_acc():
+    user = input("Input new user: ")
+    pw = getpass.getpass()
+    salt = crypt.mksalt(crypt.METHOD_SHA512)
+    hashed_password = hashlib.sha512((pw + salt).encode()).hexdigest()
+    token = user + "/" + salt + "/" + hashed_password
+
+    with open("users.txt", "a") as myFile:
+        myFile.write(token)
+        myFile.write("\n")
+
+
 options = {
     '1': client,
     '2': server,
+    '3': create_acc,
 }
 
-choice = input("Choose from menu: \n1. Client\n2. Server\nChoice : ")
+choice = input(
+    "Choose from menu: \n1. Client\n2. Server\n3. Create Account\nChoice : ")
 options[choice]()
