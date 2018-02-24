1dd38a48a1e218f48268057f6ce11eba87a5d3da
John Ung <jung@Johns-MacBook-Pro-2.local>
2018-02-23 16:43:39 -0800

    filler

---
+++ md5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+#getting input
+    #tokenize input
+    #split it into 512 bits
+#constant declarations
+BLOCKSIZE1=512
+BLOCKSIZE2=32
+INSERTIONBITS=64
+
+#input containers declarations
+block = [][]
+open("data.txt") as input
+
+def getInput(input):
+    #while there is 
+    #get 512 bits and add it into block
+    
+#splitting into 512 bits
+
